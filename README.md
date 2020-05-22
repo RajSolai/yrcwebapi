e62b6b2c792e0c2ef73516a8213ba43c83b419c1
RajSolai <RajSolai@github.com>
2020-05-22 17:36:53 +0530

    init testing

---
+++ README.md
@@ -0,0 +1,1 @@
+yrcwebapi rest arcitecture
---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require("express");
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+
+const driverurl =
+  "mongodb+srv://solai_client:" +
+  process.env.PASSWD +
+  "@cluster0-udyz3.mongodb.net/test?retryWrites=true&w=majority";
+
+const DB_ID = "yrcsrmvec";
+
+const client = new MongoClient(driverurl, {
+  useUnifiedTopology: true,
+});
+
+const main = async () => {
+  try {
+    await client.connect();
+    // get all events
+    app.get("/events", (req, res) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      getEvents().then((data) => res.json(data));
+    });
+    // add one event
+    app.post("/events", (req, res) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+      );
+      addEvents(
+        req.body.title,
+        req.body.imgurl,
+        req.body.story,
+        req.body.links
+      ).then((succ) => res.sendStatus(200).send("ok"));
+      // default end point
+      app.use("/", (req, res) => {
+        res.send("YRC WEB API , all events are found in /events");
+      });
+    });
+  } catch (error) {
+    console.dir(error);
+  }
+};
+
+const getEvents = async () => {
+  let events;
+  await client
+    .db(DB_ID)
+    .collection("yrcevents")
+    .find({})
+    .toArray()
+    .then((data) => {
+      events = data;
+    });
+  return events;
+};
+
+const addEvents = async (title, imgurl, story, links) => {
+  let dataModel = {
+    title: title,
+    imgurl: imgurl,
+    story: story,
+    links: links,
+  };
+  await client.db(DB_ID).collection("yrcevents").insertOne(dataModel);
+};
+
+app.listen(process.env.PORT || 3001, (err) => {
+  if (!err) {
+    console.log("server running on port 3001");
+  }
+});
+
+main().catch((err) => console.log("Error occurred" + err));
